00decbe2d133e3f0f8e53e145a9f04f70fc798f1
François Gannaz <francois.gannaz@silecs.info>
2013-05-30 23:54:33 +0200

    Empty plugin

---
+++ version.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package    local
+ * @subpackage questions-simplified
+ * @copyright  2013 Silecs {@link http://www.silecs.info/societe}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version   = 2013060100;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires  = 2013051400;        // Requires this Moodle version
+$plugin->component = 'questions-simplified';       // Full name of the plugin (used for diagnostics)
+
