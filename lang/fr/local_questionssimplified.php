4111446e37b17ab976926d433ea9decf570f7b35
Guillaume Allegre <guillaume.allegre@silecs.info>
2013-09-19 17:54:56 +0200

    documentation ++

--- lang/fr/local_questionssimplified.php
+++ lang/fr/local_questionssimplified.php
@@ -6,7 +6,11 @@ $string['standardEdit'] = 'Formulaire de questions';
 $string['standardHeader'] = 'Saisie des questions avec leurs réponses';
 $string['MCQcreate'] = 'Mes questions';
 $string['courseChoice'] = 'Choix du cours';
-$string['wysiwygIntroTitle'] = 'Intro à WYSIWYG';
-$string['wysiwygIntroContent'] = 'Bien utiliser';
+$string['wysiwygIntroTitle'] = 'Consignes';
+$string['wysiwygIntroContent'] = "Rédigez ou collez ci-dessous l'ensemble de vos questions, en respectant les conventions suivantes : <ul>"
+    . "<li>en gras, les intitulés des questions</li>"
+    . "<li>sans mise en forme, les descriptifs développés des questions, si nécessaire</li>"
+    . "<li>en souligné, chacune des réponses correctes (unique ou pas)</li>"
+    . "<li>en barré, chacune des réponses fausses</li></ul>";
 $string['wysiwigtext'] = 'Le titre';
 $string['wysiwigtext_help'] = 'La doc';
