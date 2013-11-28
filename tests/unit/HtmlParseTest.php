0037503bcac055d007e0bfed0a8951c1ef10ad48
François Gannaz <francois.gannaz@silecs.info>
2013-11-28 14:57:39 +0100

    unit tests ++

--- tests/unit/HtmlParseTest.php
+++ tests/unit/HtmlParseTest.php
@@ -9,7 +9,7 @@ class HtmlParseTest extends PHPUnit_Framework_TestCase
      */
     public function testSimpleHtml($html)
     {
-        $q = sqc\Question::createFromHtml($html);
+        $q = sqc\Question::createFromHtml($html, false);
 
         $this->assertInstanceOf('\sqc\Question', $q);
         $this->assertEquals("Question", $q->title);
@@ -56,7 +56,7 @@ class HtmlParseTest extends PHPUnit_Framework_TestCase
 <p><span style="text-decoration: underline;"></span><span style="text-decoration: line-through;">Incorrect</span></p>
 <p><span style="text-decoration: underline;">Correct</span></p>
 <p><span style="text-decoration: underline;">Correct2</span></p>';
-        $q = sqc\Question::createFromHtml($html);
+        $q = sqc\Question::createFromHtml($html, false);
 
         $this->assertInstanceOf('\sqc\Question', $q);
         $this->assertEquals("Question", $q->title);
@@ -74,7 +74,7 @@ class HtmlParseTest extends PHPUnit_Framework_TestCase
      */
     public function testFullHtml($html)
     {
-        $q = sqc\Question::createFromHtml($html);
+        $q = sqc\Question::createFromHtml($html, false);
 
         $this->assertInstanceOf('sqc\Question', $q);
         $this->assertEquals("Question", $q->title);
@@ -117,7 +117,48 @@ class HtmlParseTest extends PHPUnit_Framework_TestCase
                  <p><span style="text-decoration: line-through;">Incorrect</span></p>
                  <p><span style="text-decoration: underline;">Correct1</span></p>
                  <p><span style="text-decoration: underline;">Correct2</span></p>'
-            )
+            ),
+        );
+    }
+
+    /**
+     * @dataProvider provideSpanHtmlChunks
+     */
+    public function testSpanHtmlStrippedTags($html)
+    {
+        $q = sqc\Question::createFromHtml($html);
+
+        $this->assertInstanceOf('sqc\Question', $q);
+        $this->assertEquals("Question", $q->title);
+        $this->assertEquals("Baratinlong", $q->intro);
+
+        $this->assertCount(3, $q->answers, "Wrong nomber of answers: " . print_r($q->answers, true));
+        $this->assertEquals(false, $q->answers[0]->correct);
+        $this->assertEquals("Incorrect", $q->answers[0]->content);
+        $this->assertEquals(true, $q->answers[2]->correct);
+        $this->assertEquals("Correct2", strip_tags($q->answers[2]->content));
+    }
+
+    /**
+     * Return chunks of HTML with the same Question content, but different formattings.
+     */
+    public function provideSpanHtmlChunks()
+    {
+        return array(
+            array( // parameters of the first call
+                '<p><strong>Question</strong></p><p>Baratin<br />long</p>
+                 <p><span style="text-decoration: line-through;">Incorrect<br /></span>
+                 <span style="text-decoration: underline;">Correct1<br />
+                 <span style="text-decoration: underline;">Correct2</span></span></p>'
+            ),
+            array( // Word-10.0 2002 + IE8 (Copier depuis Word)
+                '<p><b><span style="font-size: medium;" size="3"><span style="font-family: Times New Roman;" face="Times New Roman">Question</span></span></b></p>
+                <p><span style="font-family: Times New Roman; font-size: medium;" size="3" face="Times New Roman">Baratin<br />long</span></p>
+                <p><span style="text-decoration: line-through;"><span style="font-size: medium;" size="3"><span style="font-family: Times New Roman;" face="Times New Roman">Incorrect</span></span></span></p>
+                <p><span style="text-decoration: underline;"><span style="font-size: medium;" size="3"><span style="font-family: Times New Roman;" face="Times New Roman">Correct1</span></span></span></p>
+                <p><span style="text-decoration: underline;"><span style="font-size: medium;" size="3"><span style="font-family: Times New Roman;" face="Times New Roman">Correct2</span></span></span></p>
+                <p><span style="text-decoration: underline;"><span style="font-family: Times New Roman; font-size: medium;" size="3" face="Times New Roman"> </span></span></p>'
+            ),
         );
     }
 
@@ -126,7 +167,7 @@ class HtmlParseTest extends PHPUnit_Framework_TestCase
      */
     public function testMultiHtml($html)
     {
-        $qs = sqc\Question::createMultiFromHtml($html);
+        $qs = sqc\Question::createMultiFromHtml($html, false);
 
         $this->assertCount(2, $qs, "Wrong nomber of questions: "); //  . print_r($qs, true)
         $this->assertInstanceOf('sqc\Question', $qs[0]);
@@ -170,7 +211,7 @@ class HtmlParseTest extends PHPUnit_Framework_TestCase
                  <p><b>Question2</b></p>
                  <p><span style="text-decoration: line-through;">Incorrect</span></p>
                  <p><span style="text-decoration: underline;">Correct</span></p>'
-            )
+            ),
         );
     }
 }
