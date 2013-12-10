b14a3abed100e4ef777669ac24f0e901155f45f2
François Gannaz <francois.gannaz@silecs.info>
2013-12-10 16:24:12 +0100

    lib: s/require/require_once/g

--- lib.php
+++ lib.php
@@ -6,9 +6,9 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-require __DIR__ . '/models/Question.php';
-require __DIR__ . '/models/Answer.php';
-require __DIR__ . '/locallib.php';
+require_once __DIR__ . '/models/Question.php';
+require_once __DIR__ . '/models/Answer.php';
+require_once __DIR__ . '/locallib.php';
 
 
 // doc https://moodle.org/mod/forum/discuss.php?d=170325#yui_3_7_3_2_1359043225921_310
